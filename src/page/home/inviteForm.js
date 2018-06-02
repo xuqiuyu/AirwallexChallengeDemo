f93e23cc05e7550768381f3ee3efdc086a2425c8
xuqiuyu <xuqiuyu@zhongan.com>
2018-06-02 22:49:29 +0800

    fix some spelling errors

--- src/page/home/inviteForm.js
+++ src/page/home/inviteForm.js
@@ -86,7 +86,7 @@ const InviteForm = (props) => {
           <hr />
         </div>
         <p>You will be one of the first to experience </p>
-        <p>Broccoli & Co. when we lanch.</p>
+        <p>Broccoli & Co. when we launch.</p>
         <button className={style.submitButton} onClick={onOk}>OK</button>
       </div>
 
