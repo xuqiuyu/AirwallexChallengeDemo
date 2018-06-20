85fb8d961357f49c7b86778b4dce8782f4f78cd8
xuqiuyu <xuqiuyu@zhongan.com>
2018-06-20 18:45:21 +0800

    extend unit test

--- __tests__/Test.test.js
+++ __tests__/Test.test.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { configure, shallow } from 'enzyme';
+import { configure, shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 // time is limited , so just give one test case
 import TestComp from '../src/component/testComp';
-
 configure({ adapter: new Adapter() });
 
 const setup = () => {
@@ -13,7 +12,6 @@ const setup = () => {
     onClick: jest.fn()
   };
 
-  // shallow create Comp
   const wrapper = shallow(<TestComp {...props} />);
   return {
     props,
@@ -22,10 +20,56 @@ const setup = () => {
 };
 
 
-describe('test TestComp', () => {
+describe('TestComp Component test', () => {
   const { wrapper, props } = setup();
 
-  it('TestComp Component should be rendered', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  it('could be rendered', () => {
     expect(wrapper.find('button').exists());
   });
+
+  it('renders the correct text', () => {
+    const onClick = jest.fn();
+    const wrapper = shallow(
+      <TestComp
+        text="Hello World"
+        onClick={onClick}
+      />
+    );
+    expect(wrapper.find(".test").text()).toEqual('Hello World')
+  });
+
+  it('could be click', () => {
+    const onClick = jest.fn();
+    const wrapper = mount(
+      <TestComp
+        text="Warning Text Warning Text Warning TextW arning Text Warning Text Warning TextWarning Text"
+        onClick={onClick}
+      />
+    );
+    wrapper.find('.test').simulate('click');
+    expect(onClick).toBeCalled();
+  });
+
+  it('should render the hidden extraInfo block if state.showExtraInfo is true', () => {
+    const onClick = jest.fn();
+    const wrapper = shallow(
+      <TestComp
+        text="Hello World"
+        onClick={onClick}
+      />
+    );
+    wrapper.setState({ showExtraInfo: true });
+    expect(wrapper.find('.test_hidden').length).toBe(1);
+
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
 });
+
+
--- src/component/testComp/index.js
+++ src/component/testComp/index.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { Component as C } from 'react';
 import { string, func } from 'prop-types';
 
-const TestComp = ({ text, onClick }) => (
-  <button onClick={onClick}>
-    {text}
-  </button>
-);
+class TestComp extends C {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showExtraInfo: false
+    };
+  }
+
+  render() {
+    const { text, onClick } = this.props;
+    const { showExtraInfo } = this.state;
+
+    return (
+      <div>
+        <button className='test' onClick={onClick}>
+          {text}
+        </button>
+        {
+        showExtraInfo ? <div className='test_hidden'>Something hidden here ...</div> : ''
+      }
+      </div>
+    );
+  }
+}
 
 TestComp.propTypes = {
   text: string,
